218ab8abd3b37c153df1b1b8ae667f717858163d
Stadnik Andrey <stadnikandreypublic@gmail.com>
2023-04-08 14:34:50 +0300

    changes after bumping symfony version

--- EventListener/ExceptionListener.php
+++ EventListener/ExceptionListener.php
@@ -3,7 +3,7 @@
 namespace Enemis\SonataMediaLiipImagineBundle\EventListener;
 
 use Liip\ImagineBundle\Exception\ExceptionInterface;
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
@@ -11,9 +11,9 @@ use Imagine\Exception\InvalidArgumentException;
 
 class ExceptionListener
 {
-    public function onKernelException(GetResponseForExceptionEvent $event)
+    public function onKernelException(ExceptionEvent $event)
     {
-        $exception = $event->getException();
+        $exception = $event->getThrowable();
         if ($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST) {
             return;
         }
